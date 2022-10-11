0a4e8a72bdb9805eff0f7082357ce54eee8403e1
Khuako <112856450+Khuako@users.noreply.github.com>
2022-10-11 20:03:50 +0300

    Add files via upload

---
+++ task5.js
@@ -0,0 +1,20 @@
+window.addEventListener("DOMContentLoaded", function (event) {
+    console.log("DOM fully loaded and parsed");
+    function click1() {
+        let p = document.getElementById("price").value;
+        let q = document.getElementById("quantity").value;
+        let r = document.getElementById("result");
+        let forbiddenSymbol=/^(?!(0))\d+$/;
+        if(p.match(forbiddenSymbol)===null || q.match(forbiddenSymbol)===null)
+        {
+          alert("В поле введены недопустимые символы или начинается с нуля");
+          r.innerHTML="Ошибка";
+        }
+        else
+        {
+          r.innerHTML = parseInt(p) * parseInt(q);
+        }
+      }
+      let b = document.getElementById("button1");
+      b.addEventListener("click", click1);
+    });
